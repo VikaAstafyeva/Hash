8afd54cf484b2d754f9923b4adde2f14d72a8e8c
VikaAstafyeva <95017784+VikaAstafyeva@users.noreply.github.com>
2021-11-30 12:10:35 +0200

    Add files via upload

--- Hash.cpp
+++ Hash.cpp
@@ -1,157 +1,332 @@
 ﻿#include <iostream>
 #include <string>
-#include <list>
-#include <algorithm>
+#include <Windows.h>
+#define PRIME_SIZE 120
 
-// Класс "Идентификатор"
-// Поскольку это только пример, то в данном случае является просто обёрткой над
-// строкой. В реалиности может содержать много дополнительной информации, такой,
-// как тип переменной, признак используемости, уровень вложенности и так далее.
-class Identifier
+using namespace std;
+
+class NewClass
 {
 public:
-    // Задаётся имя переменной при создании
-    Identifier(const std::string& name) :
-        m_name(name)
+    NewClass* next;
+
+    int val;
+
+    NewClass()
     {
+        this->next = NULL;
     }
 
-public:
-    // Получается имя переменной
-    std::string name() const
+    NewClass(int val)
     {
-        return m_name;
+
+        this->val = val;
+        this->next = NULL;
     }
 
-private:
-    std::string m_name;
+    ~NewClass()
+    {
+
+        if (this->next != NULL)
+        {
+            delete this->next;
+        }
+    }
 };
 
-// Необходимо для поиска переменной по имени
-bool operator==(const Identifier& left, const Identifier& right)
-{
-    return left.name() == right.name();
-}
-
-// Функция, вычисляющая хэш
-// Принимает идентификатор, хэш которого надо посчитать
-// Возвращает вычисленный хэш
-size_t hash(const Identifier& id)
-{
-    // Если имя переменной составляет один символ - возвращается его код,
-    // умноженный на два
-    if (id.name().length() == 1)
-        return 2 * size_t(id.name()[0]);
-
-    // Иначе возвращается сумма кодов первых двух символов
-    return size_t(id.name()[0]) + size_t(id.name()[1]);
-}
-
-// Класс исключение "Идентификатор не найден"
-// Нужен для выдачи сообщения о том, что идентификатор не найден в таблице,
-// наружу функции поиска идентификатора
-class IDNotFoundException : std::exception
-{
-public:
-    IDNotFoundException(const std::string id_name) :
-        m_what(std::string("Identifier \'") + id_name + "\' not found!")
-    {
-    }
-
-    virtual ~IDNotFoundException() throw()
-    {
-    }
-
-public:
-    const char* what() const throw()
-    {
-        return m_what.c_str();
-    }
-
-private:
-    std::string m_what;
-};
-
-// Класс "Хэш-таблица", основанная на методе цепочек
-// Метод цепочек заключается в следующем: таблица представляет собой массив
-// связных списков фиксированного размера. Вычисленный хэш-функцией хэш является
-// индексом в этом массиве списков. Известно, что список по этому индексу будет
-// содержать все идентификаторы, для которых функция вернула одинаовый хэш.
-// Осталось только найти идентификатор в данном списке и возвратить ссылку на
-// него.
 class HashTable
 {
-public:
-    // Минимально возможное значение хэша
-    static const size_t min_hash_value = int('A') + int('0');
-    // Максимально возможное значение хэша
-    static const size_t max_hash_value = int('z') + int('z');
-    // Размер массива списков - хэш-таблицы
-    static const size_t hash_table_size = max_hash_value - min_hash_value;
+    NewClass* table[PRIME_SIZE];
 
-public:
-    // Добавление идентификатора в хэш-таблицу
-    void add(const Identifier& id)
+
+    static int hash(int  Value)
     {
-        // Добавление идентификатора в список, расположенный в таблице по
-        // индексу, вычисленному хэш-функцией (с учётом смещения)
-        m_hash_table[hash(id) - min_hash_value].push_back(id);
+
+        return Value % PRIME_SIZE;
     }
 
-    // Поиск идентификатора в таблице по имени
-    Identifier& get(const std::string& id_name)
+public:
+
+    HashTable()
     {
-        // Сохраняется ссылка на список, в котором потенциально будет
-        // расположен идентификатор (для простоты)
-        std::list<Identifier>& line = m_hash_table[hash(id_name) - min_hash_value];
-
-        // Поиск идентификаторы в списке по имени
-        std::list<Identifier>::iterator it =
-            std::find(line.begin(), line.end(), id_name);
-
-        // Если при поиске были просмотренны все элементы списка,и ни один не
-        // подошёл - сообщаем о том, что идентификатор не найден, посредством
-        // исключения
-        if (it == line.end())
-            throw IDNotFoundException(id_name);
-
-        // Если идентификатор найден - возвращаем ссылку на него
-        return *it;
+        for (int i = 0; i < PRIME_SIZE; i++)
+        {
+            table[i] = NULL;
+        }
     }
 
-private:
-    // Хэш-таблица - массив связных списков идентификаторов
-    std::list<Identifier> m_hash_table[hash_table_size];
+    ~HashTable()
+    {
+
+        for (int i = 0; i < PRIME_SIZE; i++)
+        {
+            delete table[i];
+        }
+    }
+
+
+
+    NewClass* find(int val)
+    {
+        int hashNumber = hash(val);
+        NewClass* result = table[hashNumber];
+        if (!result)
+        {
+            cout << " Елемент не знайдено" << endl;
+            cout << endl;
+            return NULL;
+        }
+        while (result->val != val)
+        {
+            if (!result->next)
+            {
+                cout << " Елемент не знайдено" << endl;
+                cout << endl;
+                break;
+            }
+            result = result->next;
+        }
+        return result;
+    }
+
+    void deleteElem(int val2)
+    {
+        int hashNumber = hash(val2);
+        NewClass* result = table[hashNumber];
+
+        if (!result)
+        {
+            cout << " Елемент не знайдено" << endl;
+            cout << endl;
+
+        }
+        else {
+
+            if (result->next == NULL)
+            {
+                cout << " Видалено елемент " << result->val << endl;
+
+                cout << endl;
+                result->val = 0;
+                result = result->next;
+                table[hashNumber] = NULL;
+                delete  result;
+            }
+            else {
+                if (result->val == val2)
+                {
+                    NewClass* result4 = result;
+                    result4->next = result->next;
+                    result4 = result4->next;
+                    table[hashNumber] = result4;
+
+                }
+                NewClass* result3 = result;
+                NewClass* result2 = result;
+                int mar33 = 1;
+                while (result3->val != val2)
+                {
+                    if (result3->next == 0)
+                    {
+                        cout << " Елемент не знайдено" << endl;
+                        cout << endl;
+                        mar33 = 0;
+                        break;
+                    }
+                   
+                    else {
+
+                        result2 = result3;
+                        result3 = result3->next;
+
+                    }
+
+                }
+                if (mar33 == 1) {
+                    cout << " Видалено елемент " << result3->val << endl;
+
+                    cout << endl;
+
+                    result2->next = result3->next;
+
+                }
+            }
+
+        }
+    }
+    void push(int val)
+    {
+        int hashNumber = hash(val);
+        NewClass* pers = new NewClass(val);
+        NewClass* place = table[hashNumber];
+        if (place == NULL)
+        {
+            table[hashNumber] = pers;
+            return;
+        }
+
+        while (place->next != NULL)
+        {
+            place = place->next;
+        }
+        place->next = pers;
+    }
+
+
+
+    int numElem(int it)
+    {
+        NewClass* result = table[it];
+
+        if (!result)
+        {
+            int result2 = 0;
+            return result2;
+        }
+        else {
+            int result2 = result->val;
+            return result2;
+        }
+    }
+    // перевірка на порожність
+    int perevNulls(int it2) // 0 - елемента немає
+    {
+        NewClass* result5 = table[it2];
+        int markperev;
+        if (!result5)
+        {
+            markperev = 0;
+        }
+        else { markperev = 1; }
+        return markperev;
+    }
+
+    void vivod2(int it)
+    {
+        NewClass* result4 = table[it];
+        if (!result4) { cout << " "; }
+        else {
+            if (!result4->next)
+            {
+                cout << " ";
+            }
+            else {
+                while (result4->next != NULL)
+                {
+                    result4 = result4->next;
+                    int newNum2 = result4->val;
+                    cout << " --> " << newNum2;
+                }
+            }
+        }
+    }
+
 };
 
 int main()
 {
-    // Создаём хэш-таблицу
-    HashTable ht;
-
-    // Добавляем в неё различные идентификаторы
-    ht.add(Identifier("a"));
-    ht.add(Identifier("aa"));
-    ht.add(Identifier("if"));
-    ht.add(Identifier("fi"));
-
-    // На случай, если идентификатор не будет найден, заворачиваем код поиска
-    // идентификаторов в блок try/catch
-    try
+    HashTable newTable;
+    int numbers;
+    string name1;
+    string surname1;
+    int val1;
+    int val2;
+    string name2;
+    int zmdelElem;
+    int randElem;
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    do
     {
-        // Выводим на экран информацию о различных идентификаторах
-        std::cout << ht.get("a").name() << std::endl;
-        std::cout << ht.get("aa").name() << std::endl;
-        std::cout << ht.get("if").name() << std::endl;
-        std::cout << ht.get("fi").name() << std::endl;
-        // Проверяем случай, когда идентификатор не должен быть найден
-        std::cout << ht.get("hello").name() << std::endl;
-    }
-    catch (const IDNotFoundException& ex)
-    {
-        // Если идентификатор не найден - сообщаем об этом
-        std::cerr << ex.what() << std::endl;
-    }
+        cout << endl;
+        cout << " 1. Додати елемент " << endl;
+        cout << " 2. Пошук " << endl;
+        cout << " 3. Вивести всі елементи " << endl;
+        cout << " 4. Видалити елемент " << endl;
+        cout << " 0. Вийти\n\n";
+        cout << " Номер команди > "; cin >> numbers;
+        cout << endl;
+        switch (numbers)
+        {
+        case 1:
+        {
 
+            cout << " Введіть число > "; cin >> val1; cout << endl;
+            newTable.push(val1);
+            cout << " Додано елемент" << endl;
+            cout << " " << val1 << endl;
+
+            break;}
+
+        case 2:
+        {
+            cout << " Введіть шукане число > "; cin >> val2;
+            cout << endl;
+            NewClass* search = newTable.find(val2);
+            if (search)
+            {
+                cout << " Елемент " << search->val << " в таблиці існує " << endl;
+                cout << endl;
+            }
+        }
+
+        break;
+        case 3:
+
+        {   for (int i = 0; i <= PRIME_SIZE - 1; i++)
+        {
+            int newmarPer = newTable.perevNulls(i);
+
+            if (newmarPer == 0)
+            {
+                if (i == PRIME_SIZE - 1) {
+                    for (int i = 0; i <= PRIME_SIZE; i++) {
+                        if (i < PRIME_SIZE) {
+                            int newmarPer = newTable.perevNulls(i);
+                            if (newmarPer == 0)
+                            {
+                                continue;
+                            }
+                            else { break; }
+                        }
+                        else { cout << " Таблиця поки що пуста " << endl; }
+                    }
+                }
+
+                else { continue; }
+            }
+
+            else {
+
+                int result3 = newTable.numElem(i);
+
+                cout << " KEY = " << i << " elem = " << result3;
+                newTable.vivod2(i);
+                cout << endl;
+            }
+
+
+        }
+
+        cout << endl;
+
+        }
+        break;
+        case 4:
+        {
+            cout << " Який елемент видалити? > "; cin >> zmdelElem;
+            cout << endl;
+            newTable.deleteElem(zmdelElem);
+
+        }
+        break;
+
+
+        case 0: {break;}
+        default: { cout << endl << " Команда не визначена\n\n";
+            break;}
+        }
+    } while (numbers != 0);
+    //system("pause");
     return 0;
 }
